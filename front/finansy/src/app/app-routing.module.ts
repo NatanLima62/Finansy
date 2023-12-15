07cb550f8b96d6982864e9608a07e1a96b02bf18
NatanLima <natanlimaoliveira62@gmail.com>
2023-12-15 00:12:31 -0300

    iniciando cadastro de adm

--- front/finansy/src/app/app-routing.module.ts
+++ front/finansy/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {CadastroComponent} from "./pages/autenticacao/cadastro/cadastro.component";
-import {LoginComponent} from "./pages/autenticacao/login/login.component";
+import {CadastroAdminComponent} from "./pages/admin/autenticacao/cadastro-admin/cadastro-admin.component";
+import {CadastroComponent} from "./pages/comum/cadastro/cadastro/cadastro.component";
+import {LoginAdminComponent} from "./pages/admin/autenticacao/login-admin/login-admin.component";
+
 
 const routes: Routes = [
+  // Admin routes
+  {path: 'cadastrar-admin', component: CadastroAdminComponent},
+  {path: 'login-admin', component: LoginAdminComponent},
+
+  // Default routes
   {path: 'cadastrar', component: CadastroComponent},
-  {path: 'login', component: LoginComponent},
-  {path: '**', component: LoginComponent}
+
+  // Redirect routes
+  {path: '**', component: CadastroComponent}
 ];
 
 @NgModule({
--- front/finansy/src/app/app.module.ts
+++ front/finansy/src/app/app.module.ts
@@ -4,19 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CadastroComponent } from './pages/autenticacao/cadastro/cadastro.component';
-import { LoginComponent } from './pages/autenticacao/login/login.component';
+import { CadastroAdminComponent } from './pages/admin/autenticacao/cadastro-admin/cadastro-admin.component';
+import {CadastroComponent} from "./pages/comum/cadastro/cadastro/cadastro.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import { LoginAdminComponent } from './pages/admin/autenticacao/login-admin/login-admin.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroComponent,
-    LoginComponent
+    CadastroAdminComponent,
+    LoginAdminComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
